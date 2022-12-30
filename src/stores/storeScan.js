353bc7f6de527f92e23757f051499bb105a830dd
Patrik Sevcik <patrik.sevcik1207@gmail.com>
2022-12-30 04:37:43 +0100

    list of products from db -> Home product page not working properly, but still do

--- src/stores/storeScan.js
+++ src/stores/storeScan.js
@@ -14,6 +14,8 @@ export const useStoreScan = defineStore('storeScan', {
   state: () => {
     return {
       scan_response: [],
+      getted_product: {},
+      scanList: []
     }
   },
   actions: {
@@ -21,13 +23,35 @@ export const useStoreScan = defineStore('storeScan', {
       console.log("started")
         HTTP.get(`Scans/get_product_info?barcode=${barcode}`, config)
           .then((response) => {
-            this.scan_response = response.data
+            this.scan_response.push(response.data)
+            this.last_scan = response.data
+            this.router.replace(`product/${barcode}`)
             console.log(this.scan_response)
           })
           .catch((error) => {
             alert(error)
             console.log(error)
           })
+    },
+    getProductByBarcodeFromNCDB(barcode) {
+      HTTP.get(`Products/get_product_by_barcode?barcode=${barcode}`, config)
+        .then((response) => {
+          this.getted_product = response.data
+        })
+        .catch((error) => {
+          alert(error)
+          console.log(error)
+        })
+    },
+    getScannedProductsByAccountId(account_id) {
+      HTTP.get(`Scans/scan_histories_of_account?account_id=${account_id}`, config)
+        .then((response) => {
+          this.scanList = response.data
+        })
+        .catch((error) => {
+          alert(error)
+          console.log(error)
+        })
     }
   }
 })
