31b8e2e6cbcac184551c13d8187dbee6530b3d8f
Patrik Sevcik <patrik.sevcik1207@gmail.com>
2023-01-12 20:35:24 +0100

    barcode partial fix

--- src/views/ScannerTab.vue
+++ src/views/ScannerTab.vue
@@ -1,13 +1,13 @@
 <template>
   <ion-page>
-    <ion-content :fullscreen="true">
+    <ion-content class="scanner-tab" :fullscreen="true">
       <div class="loading-text" v-if="load"><p class="loading-text-vertical">Loading...</p></div>
-        <div class="scanner" v-if="!stop">
-          <StreamBarcodeReader
-              @decode="(a, b, c) => onDecode(a, b, c)"
-              @loaded="() => onLoaded()"
-          ></StreamBarcodeReader>
-        </div>
+      <div class="scanner" v-if="!stop">
+        <StreamBarcodeReader class="scan"
+            @decode="(a, b, c) => onDecode(a, b, c)"
+            @loaded="() => onLoaded()"
+        ></StreamBarcodeReader>
+      </div>
     </ion-content>
     
   </ion-page>
@@ -65,8 +65,23 @@ const onLoaded = () => {
   }
 
   .scanner {
-    width: 100%;
     height: 100%;
+    width: 100%;
+    position: absolute;
+    top: 0px;
+    right: 0px;
+    bottom: 0px;
+    left: 0px;
+  }
+
+  .scan {
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    top: 0px;
+    right: 0px;
+    bottom: 0px;
+    left: 0px;
   }
 </style>
 
