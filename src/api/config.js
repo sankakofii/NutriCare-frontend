c72fd4105898ec0a30580ecd46366dd3aa5a1dd3
Patrik Sevcik <patrik.sevcik1207@gmail.com>
2023-01-23 22:57:19 +0100

    final

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const HTTP = axios.create({
-  baseURL: `https://nutricarev2.azurewebsites.net/api/`,
+  baseURL: `https://nutricarev3.azurewebsites.net/api/`,
   headers: {
     NCApiKey: process.env.VUE_APP_API_KEY
   }
